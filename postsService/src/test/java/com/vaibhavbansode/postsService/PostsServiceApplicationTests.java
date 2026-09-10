03f2650da3b6b1cf902379b04e9d54f54282c818
vaibhav <vaibhav992334@gmail.com>
2026-09-10 17:17:37 +0530

    - UserService and PostsService is initiated.

---
+++ postsService/src/test/java/com/vaibhavbansode/postsService/PostsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vaibhavbansode.postsService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
