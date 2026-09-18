20a8e01a0b3444f1f27dcaf8c9d31d0d7e68c111
vaibhav <vaibhav992334@gmail.com>
2026-09-18 18:20:45 +0530

    -remianing files

---
+++ uploaderService/src/test/java/com/vaibhavbansode/uploaderService/UploaderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vaibhavbansode.uploaderService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UploaderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
